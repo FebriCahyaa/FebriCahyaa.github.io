83ba1178850b4241a7eb1c4d0674e416acfc481d
FebriCahyaa <febricahya12345@gmail.com>
2026-01-12 13:56:33 +0000

    Web: Initial web
    Signed-off-by: FebriCahyaa <febricahya12345@gmail.com>

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Simple fade-in effect
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.style.opacity = 0;
+  document.body.style.transition = "opacity 0.8s ease";
+
+  requestAnimationFrame(() => {
+    document.body.style.opacity = 1;
+  });
+});
